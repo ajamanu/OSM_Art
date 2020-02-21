ccbfdf46980e81c941bb7163b20c81b0213f1ee7
ajamanu <amanu@champequity.com>
2020-02-21 17:02:20 +1100

    Added another plot

--- OSM Art.R
+++ OSM Art.R
@@ -66,3 +66,35 @@ ggplot() +
 coord_sf(xlim = c(151.26, 151.28), 
          ylim = c(-33.895, -33.885),
          expand = FALSE)
+
+#### More Artistic Plot
+
+# https://github.com/deanmarchiori/culburra/blob/master/culburra.Rmd
+
+ggplot() +
+        geom_sf(data = streets$osm_lines,
+                inherit.aes = FALSE,
+                color = "#7fc0ff",
+                size = .4,
+                alpha = .8) +
+        geom_sf(data = small_streets$osm_lines,
+                inherit.aes = FALSE,
+                color = "#ffbe7f",
+                size = .2,
+                alpha = .6) +
+        geom_sf(data = coast$osm_lines,
+                inherit.aes = FALSE,
+                color = "#ffbe7f",
+                size = .2,
+                alpha = .5) +
+        theme_minimal() +
+        labs(caption = "Bondi Beach",
+             y = "") +
+        theme(
+                plot.background = element_rect(fill = "#E4D9C6", color = NA),
+                panel.background = element_rect(fill = "#E4D9C6",  color = NA),
+                panel.grid = element_blank(), 
+                axis.text = element_text(colour = "#C7B18A", family = "Ubuntu Mono"),
+                axis.ticks = element_blank(), 
+                panel.border = element_blank(), 
+                plot.caption = element_text(family = "Ubuntu Mono", size = 65, vjust = -0.2, color = '#C7B18A'))
